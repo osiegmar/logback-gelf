6f7e4fb41b18236e2ce1ab56c71f0914d308640f
Neeme Praks <neeme.praks@eesti.ee>
2019-12-26 13:50:01 +0200

    Fix for coding standards - Use explicit scoping instead of the default package private level

--- src/test/java/de/siegmar/logbackgelf/GelfEncoderTest.java
+++ src/test/java/de/siegmar/logbackgelf/GelfEncoderTest.java
@@ -100,7 +100,7 @@ public class GelfEncoderTest {
         assertNotNull(logMsg);
     }
 
-    static LoggingEvent simpleLoggingEvent(final Logger logger, final Throwable e) {
+    public static LoggingEvent simpleLoggingEvent(final Logger logger, final Throwable e) {
         return new LoggingEvent(
             LOGGER_NAME,
             logger,
@@ -110,7 +110,7 @@ public class GelfEncoderTest {
             new Object[]{1});
     }
 
-    static void basicValidation(final JsonNode jsonNode) {
+    public static void basicValidation(final JsonNode jsonNode) {
         assertEquals("1.1", jsonNode.get("version").textValue());
         assertEquals("localhost", jsonNode.get("host").textValue());
         assertEquals("message 1", jsonNode.get("short_message").textValue());
