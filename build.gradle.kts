313c0105506a4886b6871f9180d65de3cf8e15dc
Oliver Siegmar <oliver@siegmar.de>
2024-08-24 10:14:02 +0200

    version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     pmd
     checkstyle
     jacoco
-    id("com.github.spotbugs") version "6.0.9"
+    id("com.github.spotbugs") version "6.0.20"
 }
 
 group = "de.siegmar"
@@ -24,15 +24,15 @@ repositories {
 }
 
 dependencies {
-    api("ch.qos.logback:logback-classic:1.5.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    api("ch.qos.logback:logback-classic:1.5.7")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.assertj:assertj-core:3.25.3")
-    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.38.0")
-    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
-    testImplementation("org.bouncycastle:bcpkix-jdk18on:1.77")
-    testImplementation("org.wiremock:wiremock:3.4.2")
-    testImplementation("org.awaitility:awaitility:4.2.1")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.40.1")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    testImplementation("org.bouncycastle:bcpkix-jdk18on:1.78.1")
+    testImplementation("org.wiremock:wiremock:3.9.1")
+    testImplementation("org.awaitility:awaitility:4.2.2")
 }
 
 tasks.test {
